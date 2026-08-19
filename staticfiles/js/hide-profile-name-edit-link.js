766a4ebea0794a011c9f36c38d501d6c3b3da20e
Igor Machado <igormcastro@gmail.com>
2026-08-19 19:09:22 -0300

    Removendo pasta static do rastreamento

--- staticfiles/js/hide-profile-name-edit-link.js
+++
@@ -1,9 +0,0 @@
-/* Show or hide Profile Name edit link on mouseover */
-$( document ).ready(function() {	
-	$(".profile-name").mouseover(function(){
- 	   $(".editlink").css("display", "inline-block");
-	});
-	$(".profile-name").mouseout(function(){
-	 	   $(".editlink").css("display", "none");
-		});	
-});
--- staticfiles/js/hide-short-description-remove-link.js
+++
@@ -1,9 +0,0 @@
-/* Show and hide short description Remove link on mouseover */
-$( document ).ready(function() {	
-	$(".edit-short-description").mouseover(function(){
- 	   $(".deletelink").css("display", "inline-block");
-	});
-	$(".edit-short-description").mouseout(function(){
-	 	   $(".deletelink").css("display", "none");
-		});	
-});
